86e0fd73c6ebf2ea681b7f3d06650f8888d9074e
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-03 19:12:56 -0400

    FileInput.java

--- Programming Exercise 14.1 /FileInput.java
+++ Programming Exercise 14.1 /FileInput.java
@@ -5,12 +5,37 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class FileInput extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        // Load the image (from resources or file system)
-        Image image = new Image("file:example.jpg"); // Use "file:" for local files
+@Override
+        public void start(Stage primaryStage) {
+            try {
+                // Load the image
+                FileInputStream input = new FileInputStream("playingcarddecks/images/iconmonstr-home-6-48.png");
+                Image image = new Image("http://sample.com/res/solitaire.png", 100, 0, false, false);
+        
+                // Create an ImageView to display the image
+                ImageView imageView = new ImageView(image);
 
+                // Create a layout and add the ImageView
+                StackPane root = new StackPane();
+                root.getSolitaire().add(imageView);
+
+                // Create a scene and set it on the stage
+                // Scene scene = new Scene(root, width, height);
+                Scene scene = new Scene(root, 400, 300); // Set desired width and height
+                primaryStage.setTitle("What You Should Know About Solitaire Card Games");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
 /**
  * Important Note: All GUI elements must require to use JavaFX, (No any other resources).
+ * Make sure JavaFX is properly set up in your project (JavaFX libraries added to your build path).
  */
