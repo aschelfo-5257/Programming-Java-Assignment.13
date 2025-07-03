07cffed4fe5a924f027e70c15ebd03d9831e93df
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-03 19:22:36 -0400

    FileInput.java

--- Programming Exercise 14.1 /FileInput.java
+++ Programming Exercise 14.1 /FileInput.java
@@ -5,37 +5,41 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-@Override
-        public void start(Stage primaryStage) {
-            try {
-                // Load the image
-                FileInputStream input = new FileInputStream("playingcarddecks/images/iconmonstr-home-6-48.png");
-                Image image = new Image("http://sample.com/res/solitaire.png", 100, 0, false, false);
-        
-                // Create an ImageView to display the image
-                ImageView imageView = new ImageView(image);
+public class FileInput extends Application {
 
-                // Create a layout and add the ImageView
-                StackPane root = new StackPane();
-                root.getSolitaire().add(imageView);
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the image from a local file (ensure the path is correct)
+            Image image = new Image("file:playingcarddecks/images/iconmonstr-home-6-48.png", 100, 0, false, false);
 
-                // Create a scene and set it on the stage
-                // Scene scene = new Scene(root, width, height);
-                Scene scene = new Scene(root, 400, 300); // Set desired width and height
-                primaryStage.setTitle("What You Should Know About Solitaire Card Games");
-                primaryStage.setScene(scene);
-                primaryStage.show();
+            // Display the image
+            ImageView imageView = new ImageView(image);
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+            // Create a layout and add the ImageView
+            StackPane root = new StackPane();
+            root.getChildren().add(imageView);
+            
+            /**
+             * Create a scene and set it on the stage!
+             * Scene scene = new Scene(root, width, height);
+             */
+            Scene scene = new Scene(root, 400, 300);
+            primaryStage.setTitle("What You Should Know About Solitaire Card Games");
+            primaryStage.setScene(scene);
+            primaryStage.show();
 
-        public static void main(String[] args) {
-            launch(args);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
 /**
- * Important Note: All GUI elements must require to use JavaFX, (No any other resources).
- * Make sure JavaFX is properly set up in your project (JavaFX libraries added to your build path).
+ * Displays a local image using JavaFX.
+ * Ensure JavaFX libraries are included in your project's build path.
  */
